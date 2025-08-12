4914c76b9655b23dd88bfb6fd57e82b9f90ec738
Muhammad Bachtiar <muhammadbachtiar010902@gmail.com>
2025-08-12 14:40:44 +0700

    fix responsive

--- src/atoms/logo/index.jsx
+++ src/atoms/logo/index.jsx
@@ -44,7 +44,7 @@ const Logo = ({textColor="text-[#F3F9FB]", hoverBgColor="bg-[#226597]"}) => {
                 </div>
             ) : (
                  <Link to={"/"} className={`flex items-center px-2 py-1 space-x-3 rtl:space-x-reverse rounded-md hover:${hoverBgColor} hover:scale-105 transition transform duration-300 ease-in-out`}>
-                    <img src={logo?.value?.imageUrl ?? '/unavailablei-image.png'} className="h-10" alt="App Logo"></img>
+                    <img src={logo?.value?.imageUrl ?? '/unavailable-image.png'} className="h-10" alt="App Logo"></img>
                     <div className='flex flex-col gap-2'>
                         <span className={`self-start align-baseline text-md leading-3 tracking-tighter font-semibold uppercase ${textColor}`}>{logo?.value?.regionEntity ?? '[Judul logo belum diatur]'} </span>
                         <span className={`self-start align-baseline text-xs leading-3 font-normal italic ${textColor}`}>{(logo?.value?.regionDescription) ??  "[Sub judul logo belum diatur]"} </span>
